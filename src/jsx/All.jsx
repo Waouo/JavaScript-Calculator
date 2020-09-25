d33d1ce264a8eec8ea3ad18e213da33b4695e17a
waouo <f5864565@gmail.com>
2020-09-25 09:59:29 +0800

    fontfamily

--- src/jsx/All.jsx
+++ src/jsx/All.jsx
@@ -13,9 +13,9 @@ class App extends React.Component {
 
         return (
             <div id='app'>
-                <main id='coculator' className="d-flex flex-wrap justify-content-center">
-                    <div id="display"></div>
-                    <button id='clear' className='jumbo' style={{backgroundColor: 'rgb(172,57,57)'}}>AC</button>
+                <main id='calculator' className="d-flex flex-wrap justify-content-center">
+                    <div id="display">12qweqwe3</div>
+                    <button id='clear' className='jumbo' style={{ backgroundColor: 'rgb(172,57,57)' }}>AC</button>
                     <button id='divide'>/</button>
                     <button id='multiply'>x</button>
                     <button id='seven'>7</button>
@@ -26,14 +26,14 @@ class App extends React.Component {
                     <button id='five'>5</button>
                     <button id='six'>6</button>
                     <button id='add'>+</button>
-                        <div className="d-flex flex-wrap justify-content-center" style={{width: '240px'}}>
-                            <button id='one'>1</button>
-                            <button id='two'>2</button>
-                            <button id='three'>3</button>
-                            <button id='zero' style={{width: '160px'}}> 0</button>
-                            <button id='decimal'>.</button>
-                        </div>
-                    <button id='equal' style={{ backgroundColor: "rgb(0,68,102)", height: '130px'}}>=</button>
+                    <div className="d-flex flex-wrap justify-content-center" style={{ width: '240px' }}>
+                        <button id='one'>1</button>
+                        <button id='two'>2</button>
+                        <button id='three'>3</button>
+                        <button id='zero' style={{ width: '160px' }}> 0</button>
+                        <button id='decimal'>.</button>
+                    </div>
+                    <button id='equal' style={{ backgroundColor: "rgb(0,68,102)", height: '130px' }}>=</button>
                 </main>
             </div>
         )
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, './src'),
@@ -13,18 +15,14 @@ module.exports = {
         filename: 'index-bundle.js'
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../'
-                        }
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
                     }
-                    , "css-loader", "sass-loader"
-                ],
+                }, "css-loader", "sass-loader"],
             },
             {
                 test: /\.js$/i,
@@ -36,21 +34,35 @@ module.exports = {
                     }
                 }
             },
-             {
-                 test: /\.jsx$/i,
-                 exclude: [/node_modules/],
-                 use: {
-                     loader: 'babel-loader',
-                     options: {
-                         presets: ['@babel/preset-react', '@babel/preset-env']
-                     }
-                 }
-             },
-            
+            {
+                test: /\.jsx$/i,
+                exclude: [/node_modules/],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
+
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: 'src/[name].css' }),
+        new MiniCssExtractPlugin({
+            filename: 'src/[name].css'
+        }),
         new HtmlWebpackPlugin({
             template: 'index.html',
             filename: 'index.html'
